2fcbc2005a3f499d240185706028e7ba795e77dd
ABH1SHEK9 <51086793+ABH1SHEK9@users.noreply.github.com>
2020-04-29 15:12:56 +0530

    Add files via upload

---
+++ randomfilerenamer.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+
+
+import os,sys,random,string,glob
+
+def main():
+        
+    img_dir = os.getcwd()
+    print (img_dir)
+    data_path = os.path.join(img_dir,'*jpg')
+    
+    files = glob.glob(data_path)
+    print (files)
+    j = 0
+    used = []
+    for i in files:
+
+        j=j+4
+        random.seed(j)
+        r = random.randrange(99, 99999, 1)
+        r = randomStrings()
+        print (i,r)
+        if (r in used):
+            r = randomStrings(10)
+        used.append(r)
+        
+        os.rename(i, img_dir + "/" + str(r) + ".jpg")
+
+    print("Finished!")
+
+
+def randomStrings(sLength=7):
+    alphbt = string.ascii_lowercase
+    return ''.join(random.choice(alphbt) for i in range(sLength))
+
+if __name__ == "__main__":
+    main()
+
+
+        
+                
+
+
